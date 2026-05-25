242d2e470a7f2a57360d3b1656f803078d97164d
Melany Calderon Osorno <mcalderono@inciensa.sa.cr>
2026-05-25 14:44:11 -0600

    Installing script

---
+++ installing.sh
@@ -0,0 +1,78 @@
+echo "#######     starting the installation of the packages required for INCIENSA training #####################"
+conda config --add channels defaults
+conda config --add channels bioconda
+conda config --add channels conda-forge
+conda config --set channel_priority strict
+
+echo "############ Mamba installation ##################"
+conda install -n base -c conda-forge mamba -y
+
+echo "#######     after finished, there will be three environements created:                   #################"
+echo "######################        gubbienvs, microenv, srst2_env               ###############################"
+conda create -n gubbienvs -y
+source activate gubbienvs
+mamba install "gubbins=2.3.4" -y
+conda deactivate
+
+echo "################### gubbins=2.3.4 environment installed    ###############"
+echo "################ to activate use: conda activate gubbins #################"
+
+echo "                                                                          "
+echo "################ starting microenv creation and installation #############"
+conda create -n microenv -y
+source activate microenv
+
+mamba install "kraken2>=2.1.2" "fastqc>=0.11.9" "trim-galore=0.6.6" prokka iqtree "snp-sites>=2.5.1" "snp-dists=0.8.2" quast  bedtools "unicycler>=0.5.0" "snippy>=4.6.0" "ncbi-amrfinderplus==3.10.30" "perl>=5.32.1,<6.0a0" -y
+
+
+echo "############# microenv environment created and were installed the following packages: ##################"
+echo "########## kraken2>=2.1.2 fastqc>=0.11.9 trim-galore=0.6.6 prokka iqtree snp-sites>=2.5.1 ##############"
+echo "########### snp-dists=0.8.2 quast  bedtools seqsero2  unicycler>=0.5.0 snippy>=4.6.0 ###################"
+echo "###################### ncbi-amrfinderplus==3.10.30 perl>=5.32.1,<6.0a0 mafft==7.515 ####################"
+echo "################################### activate using: conda activate microenv ############################"
+
+echo "                                      "
+echo "                                      "
+sudo apt-get update
+sudo apt-get install roary -y
+
+conda deactivate
+echo "##################### roary were installing within microenv environnent  #######################"
+
+
+echo "################ starting ariba creation and installation #############"
+conda create -n ariba_env -y
+source activate ariba_env
+mamba install -c bioconda mummer -y
+sudo pip3 install ariba
+conda deactivate
+echo "###################### ariba_env environment created and ariba was installed ######################"
+echo "############################# activate using: conda activate ariba_env ############################"
+
+
+echo "################ starting seqsero2 creation and installation #############"
+conda create -n seqsero2_env -y
+source activate seqsero2_env
+mamba install seqsero2=1.2.1 -y
+conda deactivate
+echo "###################### seqsero2_env environment created and seqsero2 was installed ######################"
+echo "############################# activate using: conda activate seqsero2_env ############################"
+
+
+echo "######################## creating srst2 environment this will take some time #############################"
+
+conda create -n srst2_env python=2.7 -y 
+
+source activate srst2_env
+
+conda install srst2 -y
+
+conda deactivate
+echo "###################### srst2_env environment created and srst2 was installed ######################"
+echo "############################# activate using: conda activate srst2_env ############################"
+
+echo "###################### AMRFinderPlus Database download ######################"
+conda activate microenv
+amrfinder --update
+
+conda deactivate
